bf6534054898c3f284f6693b4cfdc9e6472c7d1c
asendero <AlexSend57@gmail.com>
2021-10-18 16:14:49 +0300

    change metric computation

--- hw_asr/base/base_metric.py
+++ hw_asr/base/base_metric.py
@@ -1,6 +1,27 @@
+from typing import List
+
+from torch import Tensor
+
+
 class BaseMetric:
     def __init__(self, name=None, *args, **kwargs):
         self.name = name if name is not None else type(self).__name__
+        self.metric_function = None
+        self.text_encoder = None
 
-    def __call__(self, *args, **kwargs):
-        raise NotImplementedError
+    def __call__(self, log_probs: Tensor, text: List[str], *args, **kwargs):
+        argmax_inds = log_probs.cpu().argmax(-1)
+        argmax_inds = [
+            inds[: int(ind_len)]
+            for inds, ind_len in zip(argmax_inds, kwargs["log_probs_length"])
+        ]
+        if hasattr(self.text_encoder, "ctc_decode"):
+            predictions = [self.text_encoder.ctc_decode(inds.tolist()) for
+                            inds in argmax_inds]
+        else:
+            predictions = [self.text_encoder.decode(inds.tolist()) for
+                            inds in argmax_inds]
+        ers = []
+        for pred_text, target_text in zip(predictions, text):
+            ers.append(self.metric_function(target_text, pred_text))
+        return sum(ers) / len(ers)
