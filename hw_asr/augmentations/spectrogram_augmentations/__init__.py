069106c97d1c762c790e232ac71b5e7d88602ade
asendero <AlexSend57@gmail.com>
2021-10-19 18:43:16 +0300

    fix bug with aug init

--- hw_asr/augmentations/spectrogram_augmentations/__init__.py
+++ hw_asr/augmentations/spectrogram_augmentations/__init__.py
@@ -1,1 +1,8 @@
+from hw_asr.augmentations.spectrogram_augmentations.Cutout import Cutout
+from hw_asr.augmentations.spectrogram_augmentations.RandomErasing import \
+    RandomErasing
 
+__all__ = [
+    "Cutout",
+    "RandomErasing"
+]
--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,5 +1,10 @@
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
+from hw_asr.augmentations.wave_augmentations.Noise import Noise
+from hw_asr.augmentations.wave_augmentations.SpeedPerturbation import \
+    SpeedPerturbation
 
 __all__ = [
-    "Gain"
+    "Gain",
+    "Noise",
+    "SpeedPerturbation"
 ]
