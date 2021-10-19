a8a3c8235bbd0cf2ed7c474548f38f8b28fa2e42
asendero <AlexSend57@gmail.com>
2021-10-19 23:31:05 +0300

    change speed_pert

--- hw_asr/augmentations/wave_augmentations/SpeedPerturbation.py
+++ hw_asr/augmentations/wave_augmentations/SpeedPerturbation.py
@@ -3,13 +3,18 @@ from torch import Tensor
 from hw_asr.augmentations.base import AugmentationBase
 
 import torchaudio
+import random
 
 
 class SpeedPerturbation(AugmentationBase):
-    def __init__(self, speed_x, *args, **kwargs):
-        self.speed_x = speed_x
+    def __init__(self, speed_min, speed_max, *args, **kwargs):
+        self.speed_min = speed_min
+        self.speed_max = speed_max
 
     def __call__(self, data: Tensor, sample_rate):
-        new_sample_rate = int(self.speed_x * sample_rate)
+        if random.random() > 0.5:
+            new_sample_rate = int(self.speed_max * sample_rate)
+        else:
+            new_sample_rate = int(self.speed_min * sample_rate)
         x = torchaudio.functional.resample(data, sample_rate, new_sample_rate)
         return x, new_sample_rate
