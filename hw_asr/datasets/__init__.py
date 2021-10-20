1368f02666bc115c796f7e4d534dc759f4a231a7
asendero <AlexSend57@gmail.com>
2021-10-20 21:41:15 +0300

    fix datasets init

--- hw_asr/datasets/__init__.py
+++ hw_asr/datasets/__init__.py
@@ -1,9 +1,11 @@
 from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
 from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
+from hw_asr.datasets.lj_dataset import LJDataset
 
 __all__ = [
     "LibrispeechDataset",
     "CustomDirAudioDataset",
-    "CustomAudioDataset"
+    "CustomAudioDataset",
+    "LJDataset"
 ]
