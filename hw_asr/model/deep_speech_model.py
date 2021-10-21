2cf1fef321ab3a8af87c9962e7af47c8a7b6402d
asendero <AlexSend57@gmail.com>
2021-10-22 01:46:49 +0300

    fix deep speech model

--- hw_asr/model/deep_speech_model.py
+++ hw_asr/model/deep_speech_model.py
@@ -1,4 +1,4 @@
-import math
+import torch
 from torch import nn
 
 from hw_asr.base import BaseModel
@@ -16,37 +16,30 @@ class DeepSpeechModel(BaseModel):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 96, (21, 11), stride=(2, 1), padding=(10, 5)),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(96),
             nn.ReLU()
         )
-        rnn_input_size = int(math.floor(n_feats) / 2) + 1
-        rnn_input_size = int(math.floor(rnn_input_size + 2 * 20 - 41) / 2 + 1)
-        rnn_input_size = int(math.floor(rnn_input_size + 2 * 10 - 21) / 2 + 1)
-        rnn_input_size = int(math.floor(rnn_input_size + 2 * 10 - 21) / 2 + 1)
-        rnn_input_size *= 96
+        rnn_input_size = 96 * n_feats // 8
         self.lstm = nn.LSTM(input_size=rnn_input_size, hidden_size=hidden_size,
-                            num_layers=n_layers, batch_first=False,
+                            num_layers=n_layers, batch_first=True,
                             dropout=dropout, bidirectional=True)
-        self.fully_connected = nn.Sequential(
-            nn.BatchNorm1d(hidden_size),
-            nn.Linear(hidden_size, n_class)
-        )
+        self.bn = nn.BatchNorm1d(2 * hidden_size)
+        self.fc = nn.Linear(2 * hidden_size, n_class)
 
     def forward(self, spectrogram, *args, **kwargs):
-        packed_input = nn.utils.rnn.pack_padded_sequence(
-            spectrogram, kwargs["spectrogram_length"], batch_first=True,
-            enforce_sorted=False)
-        x = self.convolutional(packed_input)
+        print(spectrogram.shape)
+        x = self.convolutional(torch.transpose(spectrogram, 1, 2).unsqueeze(1)
+                               )
         sizes = x.size()
         x = x.view(sizes[0], sizes[1] * sizes[2],
                    sizes[3])  # Collapse feature dimension
-        x = x.transpose(1, 2).transpose(0, 1).contiguous()  # TxNxH
+        x = x.transpose(1, 2)
         lstm_out, _ = self.lstm(x)
-        lstm_out, _ = nn.utils.rnn.pad_packed_sequence(lstm_out,
-                                                       batch_first=False)
-        x = self.fully_connected(lstm_out)
-        x = x.transpose(0, 1)
+        lstm_out = torch.transpose(lstm_out, 1, 2)
+        x = self.bn(lstm_out)
+        x = torch.transpose(x, 1, 2)
+        x = self.fc(x)
         return {"logits": x}
 
     def transform_input_lengths(self, input_lengths):
-        return input_lengths
+        return input_lengths // 2
