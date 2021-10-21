7602a1682c0b164e974fc45c38082edf54554f1b
asendero <AlexSend57@gmail.com>
2021-10-21 16:10:30 +0300

    change augs + config

--- hw_asr/augmentations/spectrogram_augmentations/Cutout.py
+++ hw_asr/augmentations/spectrogram_augmentations/Cutout.py
@@ -8,8 +8,8 @@ from hw_asr.augmentations.base import AugmentationBase
 class Cutout(AugmentationBase):
     def __init__(self):
         self.aug = torch.nn.Sequential(
-            torchaudio.transforms.FrequencyMasking(20),
-            torchaudio.transforms.TimeMasking(100),
+            torchaudio.transforms.FrequencyMasking(50),
+            torchaudio.transforms.TimeMasking(120),
         )
 
     def __call__(self, data: Tensor, sample_rate):
