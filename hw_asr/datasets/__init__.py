f4d4b00beeacb91f18ce522a2ad1a023df624334
asendero <AlexSend57@gmail.com>
2021-10-21 17:34:26 +0300

    add numbers dataset to init

--- hw_asr/datasets/__init__.py
+++ hw_asr/datasets/__init__.py
@@ -2,10 +2,12 @@ from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
 from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
 from hw_asr.datasets.lj_dataset import LJDataset
+from hw_asr.datasets.numbers_dataset import NumbersDataset
 
 __all__ = [
     "LibrispeechDataset",
     "CustomDirAudioDataset",
     "CustomAudioDataset",
-    "LJDataset"
+    "LJDataset",
+    "NumbersDataset"
 ]
