506a6092373e52960d8928225210bcbcbd86d476
asendero <AlexSend57@gmail.com>
2021-10-23 14:47:41 +0300

    add russian to init

--- hw_asr/datasets/__init__.py
+++ hw_asr/datasets/__init__.py
@@ -3,11 +3,13 @@ from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
 from hw_asr.datasets.lj_dataset import LJDataset
 from hw_asr.datasets.numbers_dataset import NumbersDataset
+from hw_asr.datasets.russian_dataset import RussianDataset
 
 __all__ = [
     "LibrispeechDataset",
     "CustomDirAudioDataset",
     "CustomAudioDataset",
     "LJDataset",
-    "NumbersDataset"
+    "NumbersDataset",
+    "RussianDataset"
 ]
