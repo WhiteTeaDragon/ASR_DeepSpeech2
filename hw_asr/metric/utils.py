95feec0eebdd357680ba9958ddb532f9e17e9bc0
asendero <AlexSend57@gmail.com>
2021-10-26 17:48:52 +0300

    fix cer metric

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -4,6 +4,8 @@ import editdistance
 def calc_cer(target_text, predicted_text) -> float:
     if len(target_text) == 0 and len(predicted_text) == 0:
         return 1
+    if len(target_text) == 0:
+        return 0
     return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 
